8f9e1b9fbf282045ab5b1351ad898cd7fd45da29
cindyweiss <weiss_cindy@yahoo.com>
2020-02-11 13:19:50 -0800

    now completed lab one by formatting my files to mimic the mob coding file

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # notes
+Lab - class 01
+project: Node Ecosystem
+author: Cindy Weiss
+
+links: https://github.com/cindyweiss/notes/pull/1
--- index.js
+++ index.js
@@ -3,12 +3,18 @@
 const Input = require('./lib/input.js');
 const Notes = require('./lib/notes.js');
 
-const options = new Input;
-options.valid() ? Notes.fetch(options) : help();
+const options = new Input();
+// options.valid() ? Notes.fetch(options) : help();
+const notes = new Notes(input);
+
+if (input.valid()) {
+  Notes.execute();
+} else {
+  help();
+};
+
 
 function help() {
-  console.log('well that did not t work!');
+  console.log('well that did not not work!');
   process.exit();
 }
-
-
